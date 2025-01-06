3f94ec12f71344f16987e1707fd94745d8905064
emmanuel <sandoval.morales.emmanuel@gmail.com>
2025-01-05 23:06:54 -0600

    update files

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/v1';
+const API_URL = 'http://localhost:3001/api/v1';
 
 export const api = axios.create({
   baseURL: API_URL,
   headers: {
-    'Accept': '*/*',
     'Content-Type': 'application/json',
   },
 });
